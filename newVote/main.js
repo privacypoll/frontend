eb5d0b99dd7fa54eb7149fb84164b545233304fc
koroorrarokair <40911271+kieroid@users.noreply.github.com>
2023-09-16 14:23:29 -0500

    is this working, i dont know

--- newVote/main.js
+++ newVote/main.js
@@ -1,9 +1,10 @@
-document.getElementById("voteID").onclick = function(){
-    let inputVoteID = document.getElementById("voteID")
-    let inputCandidateID = document.getElementById("candidateID")
-    let inputVoteText = inputVoteID.value;
-    let inputCandidateText = inputCandidateID.value;
 
-    inputVoteID.value = '';
-    inputCandidateID.value = '';
-};
+function sendInfo() {
+    var votersID = document.getElementById("voteID");
+    var votersVote = document.getElementById("candidateID");
+
+    votersID.value = "";
+    votersVote.value = "";
+
+
+}
