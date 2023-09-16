03ec7a21c679377b1f7fa5f7736b48f7601d51f8
koroorrarokair <40911271+kieroid@users.noreply.github.com>
2023-09-16 14:34:05 -0500

    i dont know

--- newVote/main.js
+++ newVote/main.js
@@ -1,10 +1,15 @@
+let sentMessage = false
 
 function sendInfo() {
+    if (sentMessage == false) {
+        let sentMessage == True
+    }
     var votersID = document.getElementById("voteID");
     var votersVote = document.getElementById("candidateID");
+
 
     votersID.value = "";
     votersVote.value = "";
 
-
+    new Request()
 }
