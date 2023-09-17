f4a3a4f37d234fac6d7d1da5006a7b04229886fd
Palomarrrr <ieatrocks4fun123@gmail.com>
2023-09-17 03:21:04 -0500

    g

--- newVote/scripts/httpRequests.js
+++ newVote/scripts/httpRequests.js
@@ -11,14 +11,18 @@ async function putIdentity(voteId, identityString){
     const url = "https://put-identity.tjroh01.workers.dev";
     const keyPair = await window.crypto.subtle.generateKey({name: "ECDSA", namedCurve: "P-256",}, true, ["sign", "verify"])
     const pubKey = btoa(JSON.stringify(await window.crypto.subtle.exportKey("jwk", keyPair.publicKey)));
+    const headers = new Headers();
+    headers.append("vote-id", voteId.toString())
 
+    console.log(JSON.parse(JSON.stringify({publicKey: pubKey, identityString: identityString})))
     return fetch(url, {
         method: "POST",
-        headers: { "vote-id": voteId },
+        headers: headers,
         body: JSON.stringify({publicKey: pubKey, identityString: identityString})
-    }).then(res => {return res.json()})
+    }).then(function(res){console.log(res)})
         .catch(function(res) {console.log(res)})
 }
+
 async function getInitial(id_0) {
     const url = "https://get-initial.tjroh01.workers.dev";
     return fetch(url, {
@@ -29,4 +33,28 @@ async function getInitial(id_0) {
     })
         .then(res => {return res.json()})
         .catch(function(res){ console.log(res) });
+}
+
+async function getBlock(voteId){
+    const url = "https://get-block.tjroh01.workers.dev";
+    let headers = new Headers();
+    headers.append("vote-id", voteId.toString());
+
+    return fetch(url, {
+        method: "GET",
+        headers: headers
+    }).then(res => {return res.json()})
+        .catch(function(res) {console.log(res.json())});
+}
+
+async function getKeyRing(voteId){
+    const url = "https://get-keyring.tjroh01.workers.dev";
+    let headers = new Headers();
+    headers.append("vote-id", voteId.toString());
+
+    return fetch(url, {
+        method: "GET",
+        headers: headers
+    }).then(res => {return res.json()})
+        .catch(function (res) {console.log(res.json())});
 }
--- newVote/scripts/voteblock.js
+++ newVote/scripts/voteblock.js
@@ -1,35 +1,21 @@
-<<<<<<< HEAD
 //import * as zkp from '@cloudflare/zkp-ecdsa';
 
-=======
-async function generateKeyPair() {
-    var keyPair = await window.crypto.subtle.generateKey({
-        name: "ECDSA",
-        namedCurve: "P-256"
-    }, true, ["sign", "verify"]);
-    return (keyPair);
-};
->>>>>>> 4be520970cba7ea1c670e1264efce48eb794d08d
-function GetEncodedString(input){
-    let enc = new TextEncoder();
-    return enc.encode(input);
-}
-async function CreateVoteBlock(vote_id, vote_choice, block_id, user_key_pair, key_ring, user_key_index){
+async function CreateVoteBlock(vote_id, vote_choice, block_id, previous_block_hash, user_key_pair, key_ring){
     const block = {
         vote_id: vote_id,
-        timestamp: new Date().getDate(),
         vote_choice: vote_choice,
         block_id: block_id,
-        public_key: user_key_pair.publicKey
+        previous_block_hash: previous_block_hash,
+        public_key: btoa(JSON.stringify(window.crypto.subtle.exportKey("jwk", user_key_pair.publicKey))),
     };
 
     const block_json =  JSON.stringify(block, null, 2);
-    const signature = await window.crypto.subtle.sign( {name: "ECDSA", hash: { name: "SHA-256" } }, user_key_pair.privateKey, GetEncodedString(block_json));
+    const signature = await window.crypto.subtle.sign( {name: "ECDSA", hash: { name: "SHA-256" } }, user_key_pair.privateKey, new TextEncoder().encode(block_json));
     const params = zkp.generateParamsList();
-    const block_hash = await window.crypto.subtle.digest("SHA-256", GetEncodedString(block_json));
+    const block_hash = await window.crypto.subtle.digest("SHA-256", new TextEncoder().encode(block_json));
 
     return {
         block: block,
-        block_proof: await zkp.proveSignatureList(params, block_hash, signature, user_key_pair.publicKey, user_key_index, key_ring),
+        block_proof: await zkp.proveSignatureList(params, new Uint8Array(block_hash), new Uint8Array(signature), user_key_pair.publicKey, user_key_index, key_ring),
     };
 }
