01a6228806ebe1f8b2b65c467ef97873b9f1453d
koroorrarokair <40911271+kieroid@users.noreply.github.com>
2023-09-16 22:01:45 -0500

    the stupid one

--- httpRequests.js
+++ httpRequests.js
@@ -1,33 +1,26 @@
-async function postInitial() {
-    var url = "http://tjroh01.workers.dev"
-    const response = await fetch()
-};
-
-async function getInitial(voterID) {
+async function putInitial(id_0) {
     const url = "http://get-initial.tjroh01.workers.dev";
     fetch(url, {
-        method: "POST",
-        body: JSON.stringify({
-            "voteId": voterID
-        }),
+        method: "GET",
         headers: {
-            "vote.id":
+            "vote-id":   id_0 //this is the stupid one
         }
     })
         .then(function (res){ console.log(res)})
         .catch(function(res){ console.log(res) });
 };
 
-async function postParallel(information) {
-    const responses = await Promise.all(
-        information.map(async id => {
-            const res = await fetch(url, {
-                method: "POST",
-
-                body: JSON.stringify({
-                    "voteId": information
-                })
-            });
-        })
-    );
+async function getInitial(id_0,id_1) {
+    const url = "http://get-initial.tjroh01.workers.dev";
+    fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+            "voteId": id_0
+        }),
+        headers: {
+            "vote-id":   id_1
+        }
+    })
+        .then(function (res){ console.log(res)})
+        .catch(function(res){ console.log(res) });
 };
